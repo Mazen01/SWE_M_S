a13555ffc4b75aee51e60e9900fb671d353d5772
mazenalahmari <mzoone909@gmail.com>
2022-11-21 16:38:26 +0300

    klkjkj

--- src/sorter.java
+++ src/sorter.java
@@ -1,7 +1,53 @@
-public class sorter {
-    public static void main(String[] args) {
-        System.out.println("l;");
-    }
+public class Sorter {
+    public void selectionSort(int[] arr){
+        int n = arr.length;
 
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < n-1; i++)
+        {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+
+            // Swap the found minimum element with the first
+            // element
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    public class InsertionSort {
+        /*Function to sort array using insertion sort*/
+        void sort(int arr[]) {
+            int n = arr.length;
+            for (int i = 1; i < n; ++i) {
+                int key = arr[i];
+                int j = i - 1;
+
+            /* Move elements of arr[0..i-1], that are
+               greater than key, to one position ahead
+               of their current position */
+                while (j >= 0 && arr[j] > key) {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = key;
+            }
+        }
+
+        /* A utility function to print array of size n*/
+        static void printArray(int arr[]) {
+            int n = arr.length;
+            for (int i = 0; i < n; ++i)
+                System.out.print(arr[i] + " ");
+
+            System.out.println();
+        }
+    }
+    public void eesort (int array[]){
+        InsertionSort ob = new InsertionSort();
+        ob.sort(array);
+    }
 }
-//1QAZswedcx1
